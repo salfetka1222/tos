13a0119061a3d8df32fd3ad2ed3e6a7785209e59
salfetka1222 <salfetka1222@gmail.com>
2026-09-22 02:50:58 +0300

    Update system.py

--- core/system.py
+++ core/system.py
@@ -1,12 +1,19 @@
+from core.config import Config
+
+
 class TOS:
     def __init__(self):
-        self.name = "T-OS"
-        self.version = "0.1.0"
-        self.language = "ru"
+        self.name = Config.APP_NAME
+        self.version = Config.VERSION
+        self.language = Config.DEFAULT_LANGUAGE
+        self.environment = Config.ENVIRONMENT
+        self.debug = Config.DEBUG
 
     def info(self):
         return {
             "name": self.name,
             "version": self.version,
             "language": self.language,
+            "environment": self.environment,
+            "debug": self.debug,
         }
