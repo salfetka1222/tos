1c227d68e3b6bf2755fe06a8f8031f4f8dab9904
salfetka1222 <salfetka1222@gmail.com>
2026-09-22 02:58:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from core.system import TOS
 from core.users import UserManager
+from database.database import Database
 
 
 def main():
     system = TOS()
     users = UserManager()
+    database = Database()
+
+    database.initialize()
 
     user = users.create_user(
         user_id=1,
@@ -17,14 +21,15 @@ def main():
     print(f"Версия: {system.version}")
     print(f"Язык: {system.language}")
 
+    print("\n💾 Database")
+    print("SQLite: OK")
+
     print("\n👤 Пользователь")
     print(f"ID: {user.user_id}")
     print(f"Username: @{user.username}")
     print(f"Уровень: {user.level}")
     print(f"XP: {user.xp}")
     print(f"Монеты: {user.coins}")
-
-    print(f"\nВсего пользователей: {users.count()}")
 
 
 if __name__ == "__main__":
