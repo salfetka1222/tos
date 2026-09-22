84971c30edd62d71693d3708b1d653ac6d952264
salfetka1222 <salfetka1222@gmail.com>
2026-09-22 22:27:06 +0300

    Update __init__.py

--- telegram/handlers/__init__.py
+++ telegram/handlers/__init__.py
@@ -1,1 +1,17 @@
-.
+def __init__(self, bot, database):
+    self.bot = bot
+    self.database = database
+
+    self.games = GamesSystem(database)
+    self.developer = DeveloperSystem(database)
+    self.group_os = GroupOS(bot, database)
+
+    self.group = GroupHandler(
+        bot,
+        database,
+        self.group_os
+    )
+
+    self.file_states = {}
+    self.terminal_dirs = {}
+    self.command_history = {}
